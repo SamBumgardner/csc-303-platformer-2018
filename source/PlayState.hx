955e68a5d6e91cdaa5a1b8c28ae1794316f911c7
SGF\ted1996 <aguynamedteddy@gmail.com>
2018-04-02 17:46:58 -0500

    test files for our first haxe experience

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,33 @@
 package;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.util.FlxColor;
+import flixel.math.FlxRandom;
+
 
 class PlayState extends FlxState
 {
 	override public function create():Void
 	{
 		super.create();
+		
+		var RNG:FlxRandom = new FlxRandom();
+	
+		var k = 0;
+		while (k <= 100) {
+			
+			var ranX:Float = RNG.float(0, 640);
+			var ranY:Float = RNG.float(0, 480);
+			var thing:FlxSprite = new FlxSprite(ranX, ranY);	
+			add(thing);
+			k++;
+		}
+		
+		var thing1:FlxSprite = new FlxSprite(30, 30);
+		thing1.makeGraphic(580, 70, FlxColor.CYAN);
+		add(thing1);
+		
 	}
 
 	override public function update(elapsed:Float):Void
