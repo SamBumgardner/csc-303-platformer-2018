148f6eb322c9e87e5f14b65fc58efd29d8469061
SGF\ted1996 <aguynamedteddy@gmail.com>
2018-04-02 18:39:25 -0500

    more changes to asn2

--- source/Hero.hx
+++ source/Hero.hx
@@ -2,34 +2,22 @@ package;
 
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.math.FlxPoint;
 import flixel.util.FlxColor;
 import flixel.math.FlxRandom;
 
-/**
- * ...
- * @author Ted Green
- * Assignment 2-2
- * 
-1. Create a new file in the project's source directory called Hero.hx.
-2. In that file, create the Hero class that extends FlxSprite.
-3. Inside the Hero's new() function, use makeGraphic() to give it a simple rectangle graphic.
-4. Inside the Hero's update() function, set the Hero objects color to a value determined by screen position.
-		Hint: Look at the PlayState class for an example of overriding and update function.
-6. Make Hero-class objects move automatically each frame.
-7. Randomize the direction of its movement when it is created.
-8. Add multiple Hero objects to the PlayState.
- */
 class Hero extends FlxSprite
 {
-
 	public function create()
 	{
+		super.create();
+		
 		var count = 0;
 		var RNG:FlxRandom = new FlxRandom();
 		
 		while (count <= 5){
 			
-			var randX:Float = RNG.float(0, 640);
+			var randX:Float = RNG.float(0, 630);
 			var randY:Float = RNG.float(0, 480);
 			
 			var speedX:Float = RNG.float( -5, 5);
@@ -40,7 +28,7 @@ class Hero extends FlxSprite
 			
 			hero.velocity(speedX, speedY);
 			
-			add(Hero);
+			add(hero);
 			count++;
 		}
 		
