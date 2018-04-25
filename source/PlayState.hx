cf38fd33eecf3edb054d1afd9300385c0c9738bd
Miranda McCoy <mirandamccoy5@gmail.com>
2018-04-25 13:19:56 -0500

    assignment_2 - Made text move (Wooo!)

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,43 @@
 package;
 
 import flixel.FlxState;
+import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
+	private var text:FlxText;
+	private var upDownDir:String = "up";
+	private var leftRightDir:String = "left";
+	private var upEdge:Float;
+	private var downEdge:Float;
+	private var leftEdge:Float;
+	private var rightEdge:Float;
+	private var speed:Int = 3;
+	private var boxSize:Int = 100;
+	
 	override public function create():Void
 	{
 		super.create();
 
-		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
-		text.screenCenter();
+		text = new flixel.text.FlxText(0, 0, 0, "Bonjour le Monde!", 24);
+		text.screenCenter(); // Makes the object's x and y parameters automatically centered.
+							 // To center this myself, I would use:
+							 // text.x = (SCREEN_WIDTH / 2) - (text.width / 2);
+							 // text.y = (SCREEN_HEIGHT / 2) - (text.height / 2);
+							 // assuming SCREEN_WIDTH and SCREEN_HEIGHT were vars set somewhere,
+							 // but why reinvent the wheel?
 		add(text);
+		
+		// Get text's points while centered
+		var textOriginX = text.x;
+		var textOriginY = text.y;
+		
+		// Set max movement
+		rightEdge = textOriginX + boxSize;
+		leftEdge = textOriginX - boxSize;
+		downEdge = textOriginY + boxSize;
+		upEdge = textOriginY - boxSize;
+		
 
 		// Add multiple hero objects
 	}
@@ -18,5 +45,27 @@ class PlayState extends FlxState
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+
+		// Up/down movement
+		if (upDownDir == "up" && text.y >= upEdge){
+			text.y -= speed;
+		} else if (upDownDir == "up"){
+			upDownDir = "down";
+		} else if (upDownDir == "down" && text.y <= downEdge){
+			text.y += speed;
+		} else if (upDownDir == "down"){
+			upDownDir = "up";
+		}
+		
+		// Left/right movement
+		if (leftRightDir == "left" && text.x >= leftEdge){
+			text.x -= speed;
+		} else if (leftRightDir == "left"){
+			leftRightDir = "right";
+		} else if (leftRightDir == "right" && text.x <= downEdge){
+			text.x += speed;
+		} else if (leftRightDir == "right"){
+			leftRightDir = "left";
+		}
 	}
 }
